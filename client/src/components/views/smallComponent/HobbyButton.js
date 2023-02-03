e14d0f0a538c71fceed2b01acf5614e771462fd8
KungSeung <nkhone123456@naver.com>
2023-02-03 20:37:30 +0900

    개씨발 하비

--- client/src/components/views/smallComponent/HobbyButton.js
+++ client/src/components/views/smallComponent/HobbyButton.js
@@ -3,42 +3,29 @@ import './HobbyButton.css';
 import { hobbyData } from '../SignUpPage/AttributeData';
 import ModalComponent from '../SignInPage/SignInModal';
 import { ContentModalComponent } from '../NavBar/NavBar';
+import styled from 'styled-components';
 
 function HobbyButton() {
-  const data = [...hobbyData];
+  const hobby = [...hobbyData];
 
-  const [btnActive, setBtnActive] = useState('');
+  const [checked, setChecked] = useState(false);
+  const [btnActive, setBtnActive] = useState([]);
 
   const [btnCnt, setBtnCnt] = useState(0);
 
-  const toggleActive = (e) => {
-    setBtnActive(e.target.value);
-  };
-
-  // 기능
-  // 1. 버튼 2개까지 누적으로 누르거나 취소가능
-  // 2. 무조건 2개는 누르게 해야됨
-  // 3. 눌르고 나서 value 값 저장해야됨
-  const btnCount = (e) => {
-    const cnt = btnCnt;
-    if (cnt <= 1) {
-      cnt += 1;
-      setBtnCnt(cnt);
-      // 클로즈 버튼 비활성화
-    }
-
-    if (cnt === 2) {
-      // 클로즈 버튼 활성화
+  const onChangeHobby = (checked, item) => {
+    if (checked) {
+      setBtnActive([...btnActive, item]);
+    } else if (!checked) {
+      setBtnActive(btnActive.filter((el) => el !== item));
     }
   };
 
-  return data.map((item, idx) => {
+  return hobby.map((item, idx) => {
     return (
-      <div className="buttonContainer">
-        {/* classname을 수정해줘야함 */}
-        <button chec value={idx} key={idx} className={'btn' + (`${idx}` === btnActive ? 'clicked' : '')} onClick={toggleActive}>
-          {item.value}
-        </button>
+      <div className="checkboxContainer">
+        <input type="checkbox" id={String(idx)} onChange={(e) => onChangeHobby(e.target.checked, e.target.value)} checked={Boolean(item.checked)}></input>
+        <label className="checkbox_text">{item.value}</label>
       </div>
     );
   });
