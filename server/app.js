e3e5fdb52998b2e3f5380db5697fa6effc8b8d1d
KungSeung <nkhone123456@naver.com>
2023-05-12 17:25:48 +0900

    multer

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const multer = require("multer");
 
 app.use(cors());
 // 웹에서 body에 데이터를 담아서 보내는데
@@ -38,10 +39,27 @@ app.listen(4000, function () {
   console.log("Example app listening on port 4000!");
 });
 
-app.post("/", (req, res) => {
-  // 보내는 변수명과 받는 변수명이 똑같아야 인식이 된다!
-  const { user, image1, image2, image3 } = req.body;
+// app.post("/users/signup", (req, res) => {
+//   // 보내는 변수명과 받는 변수명이 똑같아야 인식이 된다!
+//   const { user, image } = req.body;
 
-  console.log(user, image1, image2, image3);
-  return res.send(user);
+//   console.log(user);
+//   return res.send(user);
+// });
+
+// multer 설정
+const upload = multer({ dest: "uploads/" });
+
+// 업로드된 파일 처리하는 라우트 핸들러
+app.post("/users/signup", upload.single("basicFilePath"), (req, res) => {
+  const file = req.file; // 업로드된 파일
+
+  // 파일 처리 로직
+  console.log(file);
+
+  // 다른 데이터 처리 로직
+  const { user } = req.body;
+  console.log(user);
+
+  res.send("File uploaded successfully");
 });
