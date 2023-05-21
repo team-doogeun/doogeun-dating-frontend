b8c8987e2cb26f1de567d1e703ae9e9ba88b8691
KungSeung <nkhone123456@naver.com>
2023-05-21 16:27:35 +0900

    대영

--- client/src/components/setProxy.js
+++ client/src/components/setProxy.js
@@ -1,12 +1,24 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
+// const { createProxyMiddleware } = require("http-proxy-middleware");
 
-module.exports = function (app) {
-  //# 서버 URL or localhost:설정한포트번호
-  app.use(
-    "/",
-    createProxyMiddleware({
-      target: "http://localhost:80",
-      changeOrigin: true,
-    })
-  );
-};
+// module.exports = function (app) {
+//   //# 서버 URL or localhost:설정한포트번호
+//   app.use(
+//     "/",
+//     createProxyMiddleware({
+//       target: "http://localhost:80",
+//       changeOrigin: true,
+//     })
+//   );
+// };
+
+// 디젤엔진에서 안좋은게 나오는데 요소수랑 화학작용을 하면 덜 안좋은게 나온다
+// 그래서 디젤 차량에 꼭 필요하다. 요소수가 없으면 디젤 차량을 사용할 수 없다.
+// 호주 -> 중국 -> 한국 으로 오던게 무역전쟁으로 인해 막혔다.
+// 1. 디젤 차량 줄이기
+// 2. 다른 공급처 확보하기
+// 3. 자국에서 생산하기 -> 비용이 많이 들지만 여러 분야에서 자국 생산이 필요하다
+
+// 심볼 통일
+// 스크립트화 / 마지막에 정리를 잘해줌
+// 지시어 3글자 : 설명을 하고간다 + 강조를 해준다
+// 정확한 발음과 엑센트
