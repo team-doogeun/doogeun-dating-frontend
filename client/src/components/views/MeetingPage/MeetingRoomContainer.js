4f105456ae098d97a7ebf7fe005846e61669e285
KungSeung <nkhone123456@naver.com>
2023-05-26 02:35:13 +0900

    meeting

--- client/src/components/views/MeetingPage/MeetingRoomContainer.js
+++ client/src/components/views/MeetingPage/MeetingRoomContainer.js
@@ -1,30 +1,137 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MeetingRoomView from "./MeetingRoomView";
-import io from "socket.io-client";
+import axios from "axios";
+import { getCookieValue } from "../../Api/loginApi";
 
 const MeetingRoomContainer = () => {
-  const socket = io("http://loaclhost:8080/");
+  // const [meetings, setMeetings] = useState([]);
 
-  // 소켓 연결
-  const initSocketConnection = () => {
-    if (socket) return;
-    socket.connect();
-  };
+  // useEffect(() => {
+  //   fetchMeetings();
+  // }, []);
 
-  // 소켓 끊기
-  const disconnectSocket = () => {
-    if (socket === null || socket.connected === false) {
-      return;
+  // userId가 맞으면 그때 데이터 넘어옴
+  // 미팅방 목록 생성
+  const fetchMeetings = async () => {
+    try {
+      const userId = getCookieValue("userId");
+      const response = await axios.get(`group/${userId}`);
+      const data = response.data;
+      // setMeetings(data);
+    } catch (error) {
+      console.error("Meeting 정보 가져오기 실패:", error);
     }
-    socket.disconnect();
-    socket = undefined;
   };
+
+  // 만들기 기능
+  // 만들기 눌렀을 때 : 모달창 띄워서 userData입력받기
+  const makeRoom = async (userData) => {
+    await axios
+      .post("/group/register", {
+        userData,
+      })
+      .then((res) => {
+        console.log(
+          `남 : ${res.maleNum}, 여 : ${res.femaleNum} 방을 만들었습니다`
+        );
+      })
+      .catch((error) => {
+        console.log("방 만들기 실패 " + error);
+      });
+  };
+
+  // 입장 버튼
+  const registerIn = async (id, title) => {
+    const userId = getCookieValue("userId");
+
+    await axios
+      .post(`/group/${userId}`, {
+        id: id,
+        title: title,
+      })
+      .then((res) => {
+        console.log(res.id + "," + res.title + "에 입장했습니다");
+      })
+      .catch((error) => {
+        console.log("방에 입장하지 못함" + error);
+      });
+  };
+
+  // 나가기 기능
+  const registerOut = async (id, title) => {
+    await axios
+      .get("/group", {
+        id: id,
+        title: title,
+      })
+      .then(() => {
+        console.log("방 나가기 성공");
+      })
+      .catch((error) => {
+        console.log("방 나가기 실패" + error);
+      });
+  };
+
+  // test data
+  const meetings = [
+    {
+      id: "1",
+      title: "건전한",
+      maleNum: "3",
+      femaleNum: "3",
+      capacity: "6",
+      groupBlindCategory: "3:3",
+      groupBlindStatus: "NOT_FULL",
+      groupBlindIntroduction: "안녕하세요~",
+    },
+    {
+      id: "2",
+      title: "안전한",
+      maleNum: "3",
+      femaleNum: "3",
+      capacity: "6",
+      groupBlindCategory: "3:3",
+      groupBlindStatus: "NOT_FULL",
+      groupBlindIntroduction: "안녕하세요~",
+    },
+    {
+      id: "3",
+      title: "구라임",
+      maleNum: "3",
+      femaleNum: "3",
+      capacity: "6",
+      groupBlindCategory: "3:3",
+      groupBlindStatus: "NOT_FULL",
+      groupBlindIntroduction: "안녕하세요~",
+    },
+    {
+      id: "4",
+      title: "안녕임",
+      maleNum: "3",
+      femaleNum: "3",
+      capacity: "6",
+      groupBlindCategory: "3:3",
+      groupBlindStatus: "NOT_FULL",
+      groupBlindIntroduction: "안녕하세요~",
+    },
+    {
+      id: "5",
+      title: "ㅎㅇ임",
+      maleNum: "3",
+      femaleNum: "3",
+      capacity: "6",
+      groupBlindCategory: "3:3",
+      groupBlindStatus: "NOT_FULL",
+      groupBlindIntroduction: "안녕하세요~",
+    },
+  ];
 
   return (
     <MeetingRoomView
-      socket={socket}
-      initSocketConnection={initSocketConnection}
-      disconnectSocket={disconnectSocket}
+      meetings={meetings}
+      makeRoom={makeRoom}
+      registerIn={registerIn}
+      registerOut={registerOut}
     ></MeetingRoomView>
   );
 };
--- client/src/components/views/MeetingPage/MeetingRoomView.js
+++ client/src/components/views/MeetingPage/MeetingRoomView.js
@@ -1,60 +1,138 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import Modal from "../../Modal/LoginModal";
+
+// 카테고리 : 2대2/ 3대3
+// 왼쪽에 설명 + 방 만들기 버튼
+// 오른쪽엔 만들어진 방 2줄 그리고 아래엔 페이지 넘버링
+// 제목, 방 만든 사람 아이디, 방 만든 날짜
+// 남 2 : 여 3 이런식으로 현재인원 표기하기
+
+// 마이페이지
+// 인풋으로 모든 정부 수정 가능
+// 모달로 정보 수정
+
+// 데이터 유지 1주일(사람마다 1주일 유지 )
+// 소개팅, 미팅
+// 마이페이지 소개팅은
+// 1. 내가 좋아요 보낸 사람 : 아이디 표시
+// 2. 내가 좋아요 받은 사람 : 좋아요 누를 수 있는 버튼 추가
+// 3. 매칭된 사람
 
 // 미팅은 실시간이라 소켓통신을 기반으로 한다
 // 들어올 수도 있고 나갈 수도 있다. -> 나가는건 이미 참여했다걸 확인한 후에 나간다.(서버에 get 요청 필요해 보임)
 
 // 해야될게 디자인
 // 그리고 인원이 다 차면 호스트가 카톡 아이디를 전부 받아옴
+// 시작을 눌러야 시작됨
 // 참가할 때 : 카카오 아이디 요청(get)
 // 아이디 인증 : sessionId
 
-// 소켓 서버 주소에 맞게 변경
-
-const MeetingRoomView = ({
-  socket,
-  initSocketConnection,
-  disconnectSocket,
-}) => {
-  const [participants, setParticipants] = useState([]);
-
-  useEffect(() => {
-    // 마운트시 소켓 연결
-    initSocketConnection();
-
-    // 서버로부터 미팅 방 인원 업데이트 이벤트 리스너 등록
-    socket.on("participants:update", (updatedParticipants) => {
-      setParticipants(updatedParticipants);
-    });
-
-    // 언마운트시 소켓 연결 해제
-    return () => {
-      disconnectSocket();
-    };
-  }, []);
-
-  const addParticipant = async () => {
-    // 서버로 인원 추가 요청
-    socket.emit("participants:add");
-
-    const response = await axios.get("http://localhost:8080/~");
-  };
-
-  const removeParticipant = () => {
-    // 서버로 인원 제거 요청
-    socket.emit("participants:remove");
-  };
+const MeetingRoomView = ({ meetings, makeRoom, registerIn, registerOut }) => {
+  const [meetingModal, setMeetingModal] = useState(false);
 
   return (
-    <MeetingContainer>
-      <h1>Meeting Room</h1>
-      <p>Participants: {participants.length}</p>
-      <ParticipateButton onClick={addParticipant}>참여</ParticipateButton>
-      <RunOutBUtton onClick={removeParticipant}>나가기</RunOutBUtton>
-    </MeetingContainer>
+    <MeetingPage>
+      <MeetingRoomContainer>
+        {meetings.map((meetingData) => (
+          <MeetingCard
+            key={meetingData.id}
+            meetings={meetingData}
+            registerIn={registerIn}
+            registerOut={registerOut}
+          ></MeetingCard>
+        ))}
+      </MeetingRoomContainer>
+      <BtnContainer>
+        <MakeRoomButton>미팅방 만들기</MakeRoomButton>
+        {meetingModal && (
+          <Modal
+            CloseModal={() => {
+              setMeetingModal(!meetingModal);
+            }}
+          >
+            <MakeMeetingRoom />
+          </Modal>
+        )}
+      </BtnContainer>
+      <PageNumContainer></PageNumContainer>
+    </MeetingPage>
   );
 };
+
+const MeetingCard = ({ meetings, registerIn, registerOut }) => {
+  return (
+    <Card>
+      <CardBody>
+        <TotalNumber>{`총인원 : ${meetings.capacity}`}</TotalNumber>
+        <MaleFeMale>{`남 ${meetings.maleNum} : 여 ${meetings.femaleNum}`}</MaleFeMale>
+        <CardTitle>{`방 제목 : ${meetings.title}`}</CardTitle>
+        <CardText>{`소갯말 : ${meetings.groupBlindIntroduction}`}</CardText>
+        <ParticipateButton
+          onClick={() => {
+            registerIn(meetings.id, meetings.title);
+          }}
+        >
+          참여
+        </ParticipateButton>
+        <RunOutButton
+          onclick={() => {
+            registerOut(meetings.id, meetings.title);
+          }}
+        >
+          나가기
+        </RunOutButton>
+      </CardBody>
+    </Card>
+  );
+};
+
+const MakeMeetingRoom = styled.div``;
+
+const MeetingPage = styled.div`
+  width: 80%;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+`;
+
+const MeetingRoomContainer = styled.div`
+  width: 80%;
+  min-height: calc(100vh - 530px);
+  display: grid;
+  grid-template-columns: repeat(4, 1fr); /* 4개의 열을 동일한 너비로 설정 */
+  grid-gap: 30px;
+  margin: 0 auto;
+  justify-content: center;
+  align-items: center;
+  background: #ffffff;
+  color: #ffffff;
+`;
+
+const Card = styled.div`
+  width: 220px;
+  height: 220px;
+  border: 1px solid #000;
+  border-radius: 1rem;
+  padding: 10px;
+  color: black;
+`;
+
+const CardBody = styled.div`
+  margin: 10px;
+`;
+
+const TotalNumber = styled.div``;
+
+const CardTitle = styled.div`
+  align-items: center;
+  font-weight: 600;
+`;
+
+const CardText = styled.p``;
+
+const MaleFeMale = styled.div``;
 
 const buttonStyles = `
   font-size: 0.8rem;
@@ -66,16 +144,6 @@ const buttonStyles = `
   &:hover {
     transform: scale(1.05);
   }
-`;
-
-const MeetingContainer = styled.div`
-  width: 100%;
-  min-height: calc(100vh - 530px);
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  background: #ffffff;
-  color: #ffffff;
 `;
 
 const ParticipateButton = styled.button`
@@ -90,7 +158,7 @@ const ParticipateButton = styled.button`
   }
 `;
 
-const RunOutBUtton = styled.button`
+const RunOutButton = styled.button`
   border: none;
   font-weight: 700;
   color: #777777;
@@ -101,5 +169,24 @@ const RunOutBUtton = styled.button`
     color: #ff4572;
   }
 `;
+
+const BtnContainer = styled.div`
+  display: flex;
+`;
+
+const MakeRoomButton = styled.button`
+  margin-left: auto;
+  border: none;
+  font-weight: 700;
+  color: #777777;
+  background: transparent;
+  ${buttonStyles}
+
+  &:hover {
+    color: #ff4572;
+  }
+`;
+
+const PageNumContainer = styled.div``;
 
 export default MeetingRoomView;
